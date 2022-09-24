1953e1d96511a0f213950c4d34046907a3918e23
F <example@example.com>
2022-09-24 22:43:31 +0900

    環境変数設定

---
+++ src/app-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference type="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  // ここに定義した変数を追加していく
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
